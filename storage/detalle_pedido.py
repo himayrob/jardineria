ff02bb218eddb385684d7e8b39243e2bb81e102e
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-01 08:43:15 -0500

    trabajo en progreso

---
+++ storage/detalle_pedido.py
@@ -0,0 +1,10 @@
+detalle_pedido =[
+    {
+        "codigo_pedido": 0,
+        "codigo_producto": "",
+        "cantidad": 0,
+        "precio_unidad": 1.0,
+        "numero_linea": 0
+        
+    }
+]
