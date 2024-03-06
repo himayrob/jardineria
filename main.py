2fedd9339be9d2cc38db381f0508abc7b407d181
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-06 13:28:10 -0500

    no se si es por que no tengo la tabla instalada

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tabulate import tabulate
-import modules.getClients as clientes
 
+import modules.getClients as cli
 import modules.getOficina as oficina
 import modules.getEmpleados as em
 
 
-print(tabulate(em.getnombreApellidosEmailEmpleados(1), tablefmt ='grid'))
+print(tabulate(cli .getallClientesSpain(all), tablefmt ='grid'))
 
--- modules/getClients.py
+++ modules/getClients.py
@@ -1,37 +1,14 @@
 import storage.cliente as cli
 
-def getAllClientesName():
-    clienteName = []
+def getallClientesSpain(all):
+    allClientesSpain = []
     for val in cli.clientes:
-        codigoName = dict({
-            "codigo_cliente": val.get('codigo_cliente'),
-            "nombre_cliente": val.get('nombre_cliente')
-        })
-        clienteName.append(codigoName)
-    return clienteName
+        if (val.get("puesto") == "spain" ):
+            allClientesSpain.append(
+                {
+                    "spain": val.get("spain")
 
-def getOneClientCodigo(codigo):
-    for val in cli.clientes:
-        if (val.get('codigo_cliente') == codigo):
-            return{
-               "codigo_cliente": val.get('codigo_cliente'),
-               "nombre_cliente": val.get('nombre_cliente') 
-            }
-    
-def getAllClientCreditCiudad(limiteCredit, ciudad):
-    clienteCredic = []
-    for val in cli.clientes:
-        if(val.get('limite_credito') >= limiteCredit and val.get('ciudad') == ciudad):
-            clienteCredic.append(val)
-    return clienteCredic
-
-def getAllClientPaisRegionCiudad(pais, region=None, ciudad=None):
-    clientZone = []
-    for val in cli.clientes:
-        if(
-            val.get('pais') == pais and 
-            (val.get('region') == region or val.get('region') == None) and
-            (val.get('ciudad') == ciudad or val.get('ciudad') == None)
-        ):
-            clientZone.append(val)
-    return clientZone
+                }
+            )
+    return allClientesSpain
+      
