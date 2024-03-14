2fbab0ef4274eb331dde6d74d202d4823036b728
himayrob <160776152+himayrob@users.noreply.github.com>
2024-03-14 09:04:27 -0500

    ahi vamos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tabulate import tabulate
+import os
 import modules.getClients as cli
 import modules.getOficina as of
 import modules.getEmpleados as em
@@ -9,35 +9,59 @@ if __name__ == "main":
     while True:
         print(f"""
 
-                                                
-   |  /  |_       |  _ _()   ()   | |
- 
-| |/| / -
-| ' | || | |  | '| | ' / | | ' / ` | | - 
-   ||  |__|||_,| || || ||||_|| .__,||
-                                           |_|
+   _     _                           _     _               _                              
+  | |   (_)                         (_)   | |             | |                             
+  | |__  _  ___ _ ____   _____ _ __  _  __| | ___     __ _| |                             
+  | '_ \| |/ _ \ '_ \ \ / / _ \ '_ \| |/ _` |/ _ \   / _` | |                             
+  | |_) | |  __/ | | \ V /  __/ | | | | (_| | (_) | | (_| | |                             
+  |_.__/|_|\___|_| |_|\_/ \___|_| |_|_|\__,_|\___/   \__,_|_|   _            _            
+                                  | |                          | |          | |           
+  _ __ ___   ___ _ __  _   _    __| | ___   _ __  _ __ ___   __| |_   _  ___| |_ ___  ___ 
+ | '_ ` _ \ / _ \ '_ \| | | |  / _` |/ _ \ | '_ \| '__/ _ \ / _` | | | |/ __| __/ _ \/ __|
+ | | | | | |  __/ | | | |_| | | (_| |  __/ | |_) | | | (_) | (_| | |_| | (__| || (_) \__ \
+ |_| |_| |_|\___|_| |_|\__,_|  \__,_|\___| | .__/|_|  \___/ \__,_|\__,_|\___|\__\___/|___/
+                                           | |                                            
+                                           |_|                                            
 
-                        
-1. Cliente
-2. Oficina
-3. Empleado
-4. Pedidos
-5. Pagos
-6. Salir
-""")
-        opcion = int(input("\nSeleccione una de las opciones: "))
-        if opcion == 1:
-            cli.menu()
-        elif opcion == 2:
-            of.menu()
-        elif opcion == 3:
-            em.menu()
-        elif opcion == 4:
-            pe.menu()
-        elif opcion == 5:
-            pag.menu()
-        elif opcion == 6:
+            
+            1. Reportes de los productos
+            2. Guardar, Actualizar y Eliminar productos
+            0. Regresar al menu principal            
+
+            """)
+        opcion = int(input("\nseleccione una de las opciones:"))
+        if(opcion ==1):
+            repproducto.menu()
+        if(opcion ==2):
+            CRUDproducto.menu()
+        elif(opcion ==0):
             break
 
+if(__name__== "__main__"):
+        # https://patorjk.com/software/taag/#p=display&h=2&v=2&f=Slant&t=Menu%20Principal
+        while True:
+            os.system("clear")
+            print("""
+                                                            _            _             _ 
+                                         (_)          (_)           | |
+  _ __ ___   ___ _ __  _   _   _ __  _ __ _ _ __   ___ _ _ __   __ _| |
+ | '_ ` _ \ / _ \ '_ \| | | | | '_ \| '__| | '_ \ / __| | '_ \ / _` | |
+ | | | | | |  __/ | | | |_| | | |_) | |  | | | | | (__| | |_) | (_| | |
+ |_| |_| |_|\___|_| |_|\__,_| | .__/|_|  |_|_| |_|\___|_| .__/ \__,_|_|
+                              | |                       | |            
+                              |_|                       |_|       
+                  1. Cliente
+                  2. Oficina
+                  3. Empleado
+                  4. pedidos
+                  5. productos
+                  0. salir
+
+                  
+""")
+            opcion = int(input("\nseleccione una de las opciones: "))
 
 
+
+   
+     
--- modules/postProducto.py
+++ modules/postProducto.py
@@ -1,7 +1,8 @@
 import json
+from tabulate import tabulate
+import json
 import requests
 def postProducto(producto):
     peticion = requests.post("",data=json.dumps())
     res=peticion.json
-    return res
-
+    
